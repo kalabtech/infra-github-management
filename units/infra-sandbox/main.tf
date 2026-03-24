0c11fd650c793d68f1a8c4e0b7f696571bb8734c
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-24 14:20:14 +0000

    feat(infra): add var repo name and wire settings module

---
+++ units/infra-sandbox/main.tf
@@ -0,0 +1,4 @@
+module "isa" {
+  source = "../../modules/repo-settings"
+  repository_name = var.repository_name
+}
--- units/infra-sandbox/variables.tf
+++ units/infra-sandbox/variables.tf
@@ -7,3 +7,8 @@ variable "github_token" {
   description = "Github access token"
   type        = string
 }
+
+variable "repository_name" {
+  description = "Name of the existing github repository"
+  type = string
+}
