2b8b44c7d68f830a3914953e4224773e05b4f2db
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-26 11:19:50 +0000

    feat(modules): refactor and set multiples rulesets

--- modules/repo-settings/rulesets.tf
+++ modules/repo-settings/rulesets.tf
@@ -2,17 +2,17 @@ data "github_repository" "this" {
   name = var.repository_name
 }
 
-resource "github_repository_ruleset" "main" {
-  count = var.protect_main && data.github_repository.this.visibility == "public" ? 1 : 0
+resource "github_repository_ruleset" "this" {
+  for_each = data.github_repository.this.visibility == "public" ? var.rulesets : {}
 
-  name        = "main"
+  name        = each.key
   repository  = data.github_repository.this.name
   target      = "branch"
   enforcement = "active"
 
   conditions {
     ref_name {
-      include = ["refs/heads/main"]
+      include = ["refs/heads/${each.value.target_branch}"]
       exclude = []
     }
   }
@@ -24,48 +24,13 @@ resource "github_repository_ruleset" "main" {
     required_signatures     = true
 
     dynamic "required_status_checks" {
-      for_each = length(var.required_checks) > 0 ? [1] : []
+      for_each = length(each.value.required_checks) > 0 ? [1] : []
       content {
         dynamic "required_check" {
-          for_each = var.required_checks
+          for_each = each.value.required_checks
+          iterator = check
           content {
-            context        = required_check.value
-            integration_id = 15368
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "github_repository_ruleset" "dev" {
-  count = var.protect_dev && data.github_repository.this.visibility == "public" ? 1 : 0
-
-  name        = "dev"
-  repository  = data.github_repository.this.name
-  target      = "branch"
-  enforcement = "active"
-
-  conditions {
-    ref_name {
-      include = ["refs/heads/dev"]
-      exclude = []
-    }
-  }
-
-  rules {
-    creation                = true
-    deletion                = true
-    required_linear_history = true
-    required_signatures     = true
-
-    dynamic "required_status_checks" {
-      for_each = length(var.required_checks) > 0 ? [1] : []
-      content {
-        dynamic "required_check" {
-          for_each = var.required_checks
-          content {
-            context        = required_check.value
+            context        = check.value
             integration_id = 15368
           }
         }
