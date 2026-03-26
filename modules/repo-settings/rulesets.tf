ab242d43ecf25aa3051b9d8b1954c5757cf14183
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-26 12:32:57 +0000

    feat(module): add required pull request options

--- modules/repo-settings/rulesets.tf
+++ modules/repo-settings/rulesets.tf
@@ -24,6 +24,15 @@ resource "github_repository_ruleset" "this" {
     required_signatures     = each.value.required_signatures
     non_fast_forward        = true
 
+    dynamic "pull_request" {
+      for_each = each.value.require_pull_request == null ? [] : [1]
+      content {
+        required_approving_review_count = each.value.require_pull_request.required_approvals
+        dismiss_stale_reviews_on_push   = each.value.require_pull_request.dismiss_stale_reviews_on_push
+        require_last_push_approval      = each.value.require_pull_request.require_last_push_approval
+      }
+    }
+
     dynamic "required_deployments" {
       for_each = length(each.value.deployment_environments) > 0 ? [1] : []
       content {
--- modules/repo-settings/variables.tf
+++ modules/repo-settings/variables.tf
@@ -11,6 +11,11 @@ variable "rulesets" {
     required_linear_history = optional(bool, false)
     deployment_environments = optional(list(string), [])
     required_checks         = optional(list(string), [])
+    require_pull_request = optional(object({
+      dismiss_stale_reviews_on_push = optional(bool, false)
+      require_last_push_approval    = optional(bool, false)
+      required_approvals            = optional(number, 0)
+    }), null)
   }))
   default = {}
 }
