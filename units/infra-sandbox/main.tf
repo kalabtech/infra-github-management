223d549b5b8fe5ab63e6b84cfa81f6afad434b59
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-26 18:01:58 +0000

    feat(units): wire gha module and set tfvars.example

--- units/infra-sandbox/main.tf
+++ units/infra-sandbox/main.tf
@@ -1,5 +1,11 @@
 module "repo-rulesets" {
   source          = "../../modules/repo-rulesets"
   repository_name = var.repository_name
-  rulesets = var.rulesets
+  rulesets        = var.rulesets
+}
+
+module "repo-gha" {
+  source               = "../../modules/repo-gha"
+  repository_name      = var.repository_name
+  environment_creation = var.environment_creation
 }
--- units/infra-sandbox/variables.tf
+++ units/infra-sandbox/variables.tf
@@ -31,3 +31,15 @@ variable "rulesets" {
   }))
   default = {}
 }
+
+# Variable - Repo GHA Module
+variable "environment_creation" {
+  type = map(object({
+    prevent_self_review = optional(bool, false)
+    wait_timer          = optional(number, 0)
+    reviewers           = optional(number, 0)
+    branch_policy = optional(object({
+      protected_branches = optional(bool, false)
+    }), null)
+  }))
+}
